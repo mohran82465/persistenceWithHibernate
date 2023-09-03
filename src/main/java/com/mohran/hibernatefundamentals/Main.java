a56cfd4d14e9966c8032c9391889588daeea4230
mohran <mohamed01098187312@gmail.com>
2023-09-04 01:30:46 +0300

    that's it

--- src/main/java/com/mohran/hibernatefundamentals/Main.java
+++ src/main/java/com/mohran/hibernatefundamentals/Main.java
@@ -1,7 +1,7 @@
 package com.mohran.hibernatefundamentals;
 
-import com.mohran.hibernatefundamentals.airport.Payment;
-import com.mohran.hibernatefundamentals.airport.Ticket;
+import com.mohran.hibernatefundamentals.airport.Address;
+import com.mohran.hibernatefundamentals.airport.Passenger;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -14,18 +14,16 @@ public class Main {
         EntityManager em = emf.createEntityManager();
 
         em.getTransaction().begin();
+        Passenger mohran = new Passenger("mohamed mohran") ;
+        Address address = new Address();
 
-        Ticket ticket = new Ticket();
-        ticket.setNumber("AA12345");
-        ticket.setOrigin("Sohag");
-        ticket.setDestination("London");
-        Payment payment = new Payment();
-        payment.setTicket(ticket);
-        payment.setAmount(200);
-        em.persist(ticket);
-        em.persist(payment);
-
-
+        address.setStreet("Akhmim st");
+        address.setNumber("7");
+        address.setZipCode("1234567");
+        address.setCity("Sohag");
+        mohran.setAddress(address);
+        em.persist(mohran);
+        
 
         em.getTransaction().commit();
         emf.close();
