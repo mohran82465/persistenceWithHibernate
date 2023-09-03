7fdc5e5e65c9d66b45e51e6870dd2f8bcf930f4a
mohran <mohamed01098187312@gmail.com>
2023-09-04 00:25:20 +0300

    join tables on one column and one to one relationship

--- src/main/java/com/mohran/hibernatefundamentals/Main.java
+++ src/main/java/com/mohran/hibernatefundamentals/Main.java
@@ -1,7 +1,7 @@
 package com.mohran.hibernatefundamentals;
 
-import com.mohran.hibernatefundamentals.airport.Passenger;
-import com.mohran.hibernatefundamentals.airport.Ticket;
+import com.mohran.hibernatefundamentals.airport.Department;
+import com.mohran.hibernatefundamentals.airport.Manager;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -14,28 +14,16 @@ public class Main {
         EntityManager em = emf.createEntityManager();
 
         em.getTransaction().begin();
-        Passenger mohran = new Passenger("mohamed mohran");
-        Passenger ashraf = new Passenger("ahmed ashraf");
 
-        Ticket ticket1 = new Ticket("AA1234");
-        Ticket ticket2 = new Ticket("BB5678");
-
-        mohran.addTicket(ticket1);
-        mohran.addTicket(ticket2);
-
-        ashraf.addTicket(ticket1);
-        ashraf.addTicket(ticket2);
-
-        ticket1.addPassenger(mohran);
-        ticket1.addPassenger(ashraf);
-        ticket2.addPassenger(mohran);
-        ticket2.addPassenger(ashraf);
-
+        Manager mohran = new Manager("mohamed mohran");
+        Department software = new Department();
+        software.setName("Software");
+        mohran.setDepartment(software);
 
         em.persist(mohran);
-        em.persist(ticket1);
-        em.persist(ashraf);
-        em.persist(ticket2);
+        em.persist(software);
+
+
 
         em.getTransaction().commit();
         emf.close();
